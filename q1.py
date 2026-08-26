76b4697bf7945b9a1888ae3a882bfc9aaa258189
Advik-Ganapathy-M-B <f20260354@pilani.bits-pilani.ac.in>
2026-08-26 12:13:02 +0530

    Q1 Completed with other 3 subparts

--- q1.py
+++ q1.py
@@ -7,13 +7,26 @@ else:
     high=0 #to store index of current highest element
     low=0 #to store index of current lowest element
     total=0 #to store sum of all integers
+    evesum=0 #to store number of even numbers
+    oddsum=0 #to store number of odd numbers
+    revnumbers=[] #to store the numbers in reverse
     for i in range(0,n):
         if numbers[i]>numbers[high]:
             high=i #for subpart 1
         if numbers[i]<numbers[low]:
             low=i #for subpart 2
         total+=numbers[i] #for subpart 3
+        if (numbers[i]%2)==0:
+            evesum+=1
+        else:
+            oddsum+=1
+        revnumbers.append(numbers[n-i-1])
 
     print("Largest element: ",numbers[high])
     print("Smallest element: ",numbers[low])
-    print("Total sum of elements:",total)
+    print("Total sum of elements: ",total)
+    print("Number of even elements: ",evesum)
+    print("Number of odd elements: ",oddsum)
+    print("Reversed list:",end=' ')
+    for i in range(0,n):
+        print(revnumbers[i],end= ' ')
